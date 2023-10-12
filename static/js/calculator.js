32f1d158eca3894b6bcb51dad973603d46cd95ad
liangbing1 <liangbing1@huya.com>
2023-10-12 11:44:18 +0800

    calculator

---
+++ static/js/calculator.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const num1 = document.getElementById('num1');
+    const num2 = document.getElementById('num2');
+    const result = document.getElementById('result');
+    const addButton = document.getElementById('addButton');
+
+    addButton.addEventListener('click', function() {
+        const value1 = parseFloat(num1.value);
+        const value2 = parseFloat(num2.value);
+        if (!isNaN(value1) && !isNaN(value2)) {
+            result.textContent = 'Result: ' + (value1 + value2);
+        } else {
+            result.textContent = 'Please enter valid numbers.';
+        }
+    });
+});
