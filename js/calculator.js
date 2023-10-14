a5bf7f194a075ed74110b7a36421f60ab273d7e1
liangbingyihao <liangbingyihao@users.noreply.github.com>
2023-10-14 09:29:03 +0000

    deploy: c8f61fe8633ca5bbf58461de05257de2cb3eac92

--- js/calculator.js
+++ js/calculator.js
@@ -1,19 +1,41 @@
+function createOrderedList() {
+    const ol = document.createElement('ol'); // 创建有序列表元素
+
+    // 创建并添加带序号的列表项
+    for (let i = 1; i <= 5; i++) {
+      const li = document.createElement('li');
+      li.textContent = `Item ${i}`;
+      ol.appendChild(li);
+    }
+
+    const container = document.getElementById('result');
+    container.innerHTML = ''; // 清空容器
+    container.appendChild(ol); // 将有序列表添加到容器中
+  }
+
 document.addEventListener('DOMContentLoaded', function() {
-    const num1 = document.getElementById('num1');
-    const num2 = document.getElementById('num2');
-    const result = document.getElementById('result');
     const addButton = document.getElementById('addButton');
 
     addButton.addEventListener('click', function() {
-        const selectedValue = document.querySelector('input[name="level"]:checked');
-        if (selectedValue) {
-            var question = window.calc.genFormula(parseInt(selectedValue.value));
-            var questionStr = question["term"].replace(/'([^']+)'/g, '<span class="blue-text">$1</span>');
-            // result.textContent = question["term"];
-            result.innerHTML = questionStr
-            // result.textContent = 'This is <span class="red-text">red</span> and this is <span class="blue-text">blue</span>'
+        var level = document.querySelector('input[name="level"]:checked');
+        level = parseInt(level.value);
+        const container = document.getElementById('result');
+        container.innerHTML = ''; // 清空容器
+        if (level) {
+            const ol = document.createElement('ol'); // 创建有序列表元素
+            var  questions = [];
+            level--;
+            for (let i = 0; i < 5; i++) {
+                var question = window.calc.genFormula(level);
+                
+                const li = document.createElement('li');
+                li.innerHTML = question["term"].replace(/'([^']+)'/g, '<span class="blue-text">$1</span>');
+                ol.appendChild(li);
+                questions.push(question);
+              }
+            container.appendChild(ol); // 将有序列表添加到容器中
           } else {
-            result.textContent = "No option selected.";
+            container.textContent = "No option selected.";
           }
     });
 });
