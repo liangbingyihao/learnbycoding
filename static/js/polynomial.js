22ce77abb1dde5b2c0ef1ece153ce63b993ff5b0
liangbingyihao@gmail.com <liangbing1@huya.com>
2023-10-30 23:53:24 +0800

    ~

---
+++ static/js/polynomial.js
@@ -0,0 +1,263 @@
+var  lanInThisPage;
+const cnTextForLans = {
+  answer:["答案","int."],
+  numerator:["分子","num."],
+  denominator:["分母","denom."],
+  detail:["过程","detail"]
+}
+function getTextForLan(lan,k){
+  let texts = cnTextForLans[k]
+  if(texts==null||texts==undefined){
+    return k;
+  }
+  if(lan=="cn"){
+    return texts[0];
+  }else{
+    return texts[1];
+  }
+}
+
+function checkResult(force = false) {
+  // const scoreContainers = document.getElementById("score");
+  // showMessage(scoreContainers,"请先输入答案","none");
+  // 获取所有具有 "my-answer" 类名的 input 元素
+  const containers = document.querySelectorAll('.my-answer-container');
+
+  // 创建一个数组来存储值
+  var isEmpty = true, firstResult = null;
+
+  // 遍历每个匹配的 input 元素并获取其值
+  var all_checked = 0;
+  containers.forEach(function (container) {
+    var checked = container.getAttribute('data-checked');
+    if (checked == "1") {
+      ++all_checked;
+      return;
+    }
+    const main = container.querySelector('.main');
+    var myAnswer = "";
+    if (main.value !== "") {
+      myAnswer = main.value;
+    } else {
+      myAnswer = "0"
+    }
+    const numerator = container.querySelector('.numerator');
+    const denominator = container.querySelector('.denominator');
+    if (numerator != null && numerator.value !== "") {
+      if (denominator.value !== "") {
+        myAnswer += " " + numerator.value + '/' + denominator.value;
+      }
+    }
+    if (myAnswer !== "" || force) {
+      if (isEmpty) {
+        isEmpty = false;
+      }
+      container.setAttribute('data-checked', '1');
+      var correct = main.getAttribute("data-correct");
+      var result = window.calc.checkResult(correct, myAnswer);
+      console.log(correct, myAnswer, result);
+      if (result == null) {
+        container.querySelector('.score').textContent = "✅";
+      } else {
+        main.classList.add('red-text');
+        main.value = result.main;
+        if (numerator != null && result.n != "") {
+          numerator.classList.add('red-text');
+          numerator.value = result.n;
+        }
+        if (denominator != null && result.d != "") {
+          denominator.classList.add('red-text');
+          denominator.value = result.d;
+        }
+        // console.log(correct+" ❌ "+myAnswer,result);
+        if (myAnswer !== "") {
+          container.querySelector('.score').textContent = "❌";
+        }
+      }
+      main.disabled = true;
+      if (numerator != null) {
+        numerator.disabled = true;
+      }
+      if (denominator != null) {
+        denominator.disabled = true;
+      }
+    } else if (firstResult == null) {
+      firstResult = container.querySelector('.score');
+    }
+  });
+  if (!force && isEmpty && firstResult !== null) {
+    firstResult.textContent = "请先输入答案";
+  }
+  return all_checked == containers.length;
+}
+
+function showMessage(element, msg, show) {
+  //show:"none"||"block"
+  // const element = document.getElementById("message");
+  element.textContent = msg;
+  element.style.display = show;
+}
+
+function isNumeric(input) {
+  // 使用parseFloat尝试将输入解析为数字
+  const number = parseFloat(input);
+
+  // 使用isNaN来检查解析结果是否为有效的数值
+  return !isNaN(number);
+}
+
+function onStart() {
+
+  const inputs = document.querySelectorAll('input.my-answer');
+
+  if (inputs.length > 0) {
+    inputs[0].focus();
+
+    // const last = inputs[inputs.length-1];
+    // last.addEventListener("blur", function(){
+    //   // 获取当前具有焦点的元素
+    //   const focusedElement = document.activeElement;
+    //   // 检查是否是一个 <input> 元素
+    //   if (focusedElement.tagName !== "INPUT") {
+    //     checkResult();
+    //   }
+    // });
+  }
+}
+
+function onLeave(e) {
+  if (e.target.value == "") {
+    e.target.value = "0";
+  }
+}
+
+function nextInput(event, currentInput) {
+  console.log("nextInput")
+  if (event.key === 'Enter' || event.key === ' ') {
+    // 如果按下的是回车键（Enter）或空格键
+    // 在这里执行相应的操作
+    if (!isNumeric(currentInput.value.trim())) {
+      currentInput.value = "";
+      return;
+    }
+    // const container = document.getElementById('result');
+    const inputs = document.querySelectorAll('input.my-answer');
+    const currentIndex = Array.from(inputs).indexOf(currentInput);
+    var checked = 0;
+
+    if (currentIndex !== -1) {
+      var nextIndex = currentIndex + 1;
+      while (true) {
+        if (checked == inputs.length) {
+          if (checkResult()) {
+            switchQuestions();
+          }
+          return;
+        }
+        if (nextIndex == inputs.length) {
+          nextIndex = 0;
+        }
+        const next = inputs[nextIndex];
+        ++checked;
+        if (!next.disabled && next.value.trim() === '') {
+          next.focus();
+          return;
+        } else {
+          ++nextIndex;
+        }
+      }
+    }
+  }
+}
+
+function startInput(event) {
+  console.log("startInput " + event.target.disabled);
+  if (!event.target.disabled) {
+    event.target.value = "";
+  }
+}
+
+function getFormulaHtml(question) {
+  console.log(question)
+  return '<math-field readonly>'+question.polynomial+'</math-field><div><math-field></math-field></div>'
+}
+
+function switchQuestions() {
+  console.log("switchQuestionsswitchQuestions")
+  const container = document.getElementById('result');
+  var level = parseInt(document.getElementById("levelGroup").value);
+  var number = parseInt(document.getElementById("numPerGroup").value);
+  var size = parseInt(document.getElementById("sizeGroup").value);
+  container.innerHTML = ''; // 清空容器
+  if (level) {
+    const ol = document.createElement('div'); // 创建有序列表元素
+    var questions = [];
+    for (let i = 0; i < number; i++) {
+      var question = window.calc.genPolynomial(level, size);
+      question["term"] = '<span class="no-part">' + (i + 1) + '. </span>' + question["term"];
+      const li = document.createElement('div');
+      // li.innerHTML = question["term"].replace(/'([^']+)'/g, '<span class="blue-text">$1</span>')
+      // +'<p class="my-answer-container"><input class="my-answer" type="text" placeholder="答案" data-correct="'+question["resultStr"]+'"> <span class="score"></span>'
+      li.innerHTML = getFormulaHtml(question);
+
+      ol.appendChild(li);
+      questions.push(question);
+    }
+    container.appendChild(ol); // 将有序列表添加到容器中
+    onStart();
+  } else {
+    container.textContent = "No option selected.";
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  // document.getElementById('numPerGroup').addEventListener('change', function () {
+  //   const container = document.getElementById('result');
+  //   container.innerHTML = '';
+  // });
+  // document.getElementById('levelGroup').addEventListener('change', function () {
+  //   const container = document.getElementById('result');
+  //   container.innerHTML = '';
+  // });
+  // document.getElementById('sizeGroup').addEventListener('change', function () {
+  //   const container = document.getElementById('result');
+  //   container.innerHTML = '';
+  // });
+
+  document.getElementById('nextGroup').addEventListener('click', function () {
+    switchQuestions();
+  });
+
+  document.addEventListener("keydown", function(event) {
+    // 处理按键按下状态
+    console.log("按键按下：" + event.key);
+  
+    // 在这里添加按键按下时的逻辑
+  });
+  
+  document.addEventListener("keyup", function(event) {
+    // 处理按键释放状态
+    console.log("按键释放：" + event.key);
+  
+    // 在这里添加按键释放时的逻辑
+  });
+
+  document.getElementById('checkResult').addEventListener('click', function () {
+    checkResult(true);
+  });
+  
+  let lan = document.getElementById("language-select-desktop").value;
+  if(lan.indexOf("cn")>0){
+    lanInThisPage="cn";
+  }else{
+    lanInThisPage="en";
+  }
+  // document.getElementById('setting').addEventListener('click', function () {
+  //   showDialog("hahaha","aaaaaa");
+  // });
+  // console.log(`{{.Params.custom_html | safeHTML}}`)
+  //document.getElementById('toc-static').innerHTML="";
+  // switchQuestions();
+  // const clickEvent = new Event("click");
+  // document.getElementById('nextGroup').dispatchEvent(clickEvent);
+});
